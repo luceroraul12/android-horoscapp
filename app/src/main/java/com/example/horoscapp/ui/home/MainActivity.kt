49fd96165d497cc91075d9f79969e347cabd5584
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-04 08:40:30 -0300

    1ro

---
+++ app/src/main/java/com/example/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.horoscapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.horoscapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
