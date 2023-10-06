e6011b7f3b74e12650ab596622999250ac970b1f
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-06 08:24:13 -0300

    primer recyclerview

---
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -0,0 +1,16 @@
+package com.example.horoscapp.ui.horoscope.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.horoscapp.databinding.ItemHoroscopeBinding
+import com.example.horoscapp.domain.model.HoroscopeInfo
+
+class HoroscopeViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    private val binding = ItemHoroscopeBinding.bind(view)
+    fun render(item: HoroscopeInfo){
+        val context = binding.tvHoroscope.context
+        binding.ivHoroscope.setImageResource(item.image)
+        binding.tvHoroscope.text = context.getString(item.name)
+    }
+}
